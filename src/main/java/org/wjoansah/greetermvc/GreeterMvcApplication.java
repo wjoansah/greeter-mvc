6ae8180d21eec3ff974323ebc90fea44385c146c
wjoansah <wonderful.owusu-ansah@amalitech.com>
2024-08-01 07:44:36 +0000

    initial

---
+++ src/main/java/org/wjoansah/greetermvc/GreeterMvcApplication.java
@@ -0,0 +1,13 @@
+package org.wjoansah.greetermvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeterMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreeterMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wjoansah/greetermvc/GreeterMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wjoansah.greetermvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreeterMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
